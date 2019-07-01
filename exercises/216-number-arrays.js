28e08c5b75108f79f9ae32207ef174e235d46fca
gaura-klein <gaurakklein@gmail.com>
2019-06-30 21:54:18 -0500

    216 complete, I used for loops to solve the functions,

--- exercises/216-number-arrays.js
+++ exercises/216-number-arrays.js
@@ -19,6 +19,14 @@ console.log(max([1, 234, 33, 52]));
 // Example:
 // sumNumbers([1, 4, 8]) --> 13
 
+function sumNumbers (array) {
+  let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result = result + array[i];
+    }
+    return result;
+}
+
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -30,21 +38,66 @@ console.log(max([1, 234, 33, 52]));
 // positives([-1, -2, -3]) --> []
 
 
-
+function positives (array) {
+    let positiveArray = [];
+       for (i = 0; i < array.length; i++) {
+           if (array[i] > 0) {
+               positiveArray.push(array[i])
+           }
+       }
+    return positiveArray;
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "evens" which takes an array of numbers and returns a new
 // array containing only the even numbers in the given array.
 // Hint: you may want to re-use your "isEven" function from 01-predicate-functions.js
 
+function isEven(number) {
+    if (typeof(number) != "number") {
+        return false;
+    } else if (number % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+function evens (array) {
+    let evenArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (isEven(array[i]) === true) {
+            evenArray.push(array[i])
+        }
+    }
+    return evenArray;
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "odds" which takes an array of numbers and returns a new
 // array containing only the odd numbers in the given array.
 // Hint: you may want to re-use your "isOdd" function from 01-predicate-functions.js
 
+function isOdd(number) {
+    if (typeof(number) != "number") {
+        return false;
+    } else if (number % 2 === 1 || number % 2 === -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+
+function odds (array) {
+    let oddArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (isOdd(array[i]) === true) {
+            oddArray.push(array[i])
+        }
+    }
+    return oddArray;
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "integers" which takes an array of numbers and returns a new
@@ -54,7 +107,15 @@ console.log(max([1, 234, 33, 52]));
 // Example:
 // integers([3.14, 2.4, 7, 8.1, 2]) --> [7, 2]
 
-
+function integers (array) {
+    let integerArray = [];
+    for (let i = 0;  i < array.length; i++) {
+        if (array[i] % 1 === 0) {
+            integerArray.push(array[i])
+        }
+    }
+    return integerArray;
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "squareDance" which takes an array of numbers and returns a
@@ -62,3 +123,11 @@ console.log(max([1, 234, 33, 52]));
 //
 // Example:
 // squareDance([1, 2, 3]) --> [1, 4, 9]
+
+function squareDance (array) {
+    let squaredArray = [];
+    for (let i = 0;  i < array.length; i++) {
+        squaredArray.push(array[i] * array[i])
+    }
+    return squaredArray;
+}
