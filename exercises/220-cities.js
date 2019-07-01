c8b5d53e61f05c5e483447c9209709cae182cfe7
gaura-klein <gaurakklein@gmail.com>
2019-06-30 22:53:14 -0500

    220 complete

--- exercises/220-cities.js
+++ exercises/220-cities.js
@@ -1,15 +1,31 @@
 // Write a function "coolCities" which takes an array of city Objects like such:
-// var cities = [
-//   { name: 'Los Angeles', temperature: 60.0},
-//   { name: 'Atlanta', temperature: 52.0 },
-//   { name: 'Detroit', temperature: 48.0 },
-//   { name: 'New York', temperature: 80.0 }
-// ];
+var cities = [
+  { name: 'Los Angeles', temperature: 60.0},
+  { name: 'Atlanta', temperature: 52.0 },
+  { name: 'Detroit', temperature: 48.0 },
+  { name: 'New York', temperature: 80.0 }
+];
 // and returns a new array containing only those cities whose temperature is
 // cooler than 70 degrees.
 
-
+function coolCities (array) {
+    let coolCitiesArray = [];
+    for (let i = 0;  i < array.length; i++) {
+        if (array[i].temperature < 70) {
+            coolCitiesArray.push(cities[i]);
+        }
+    }
+    return coolCitiesArray;
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "cityNames" which takes an array of city objects like the
 // above problem and returns an array of the cities names.
+
+function cityNames (array) {
+    let cityNameArray = []
+    for (let i = 0;  i < array.length; i++) {
+        cityNameArray.push(array[i].name);
+    }
+    return cityNameArray;
+}
